aa74c65ae47b59fbc0ee2300487c851c11a75400
dir-hub <royd3366@gmail.com>
2025-09-17 20:34:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Signup from './Signup';
 import StudentDashboard from './StudentDashboard';
 import TeacherDashboard from './TeacherDashboard';
 import LeaderBoard from './LeaderBoard';
+import StudentAnalysis from './StudentAnalysis';
 import './App.css';
 
 const App = () => {
@@ -39,6 +40,7 @@ const App = () => {
         <Route path="/student" element={<StudentDashboard onNavigate={handleNavigate} ecoPoints={ecoPoints} setEcoPoints={setEcoPoints} completedQuizzes={completedQuizzes} setCompletedQuizzes={setCompletedQuizzes} />} />
         <Route path="/teacher" element={<TeacherDashboard onNavigate={handleNavigate} />} />
         <Route path="/leaderboard" element={<LeaderBoard onNavigate={handleNavigate} currentUserRole={userRole} />} />
+        <Route path="/student-analysis" element={<StudentAnalysis onNavigate={handleNavigate} />} />
       </Routes>
     </div>
   );
